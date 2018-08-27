b25edde6f8325009e89dc45217f046b084e8f570
mrinalini1977 <mrinalinik@yahoo.com>
2018-08-27 15:00:06 +0530

    Thematic map added

--- Data_Anal.R
+++ Data_Anal.R
@@ -1,31 +1,19 @@
-#2Overall_Literacy (%)
-#3Fem_Literacy (%)	
-#4SC_Enrol(%)	
-#5ST_Enrol(%)
-#6OBC_Enrol (%)	
-#7Muslim_Enrol (%)	
-#8Girl_ Boy_Ratio	
-#9RTE_Score	
-#10SC_Tch (%)
-#11ST_Tch (%)	
-#12Gender_ratio_Tch 	
-#13Med_Instr_Non_Eng(%)	
-#14Med_Instr_Eng(%)
-#15Pvt_School (%)	
-#16Not_In_School(%)	
-#173to5_Can_read_1text(%)	
-#183to5_Can_Subtract (%)
-
 ##### Import libraries #####
 library(cluster)
 library(factoextra)
 library(NbClust)
 library(sqldf)
+library(rgdal)
+library(maptools)
+library(ggplot2)
+library(tmap)
+library(sp)
+library(ggmap)
+library(raster)
 
 ##### Read data #####
 
-setwd("C:/Jigsaw/education/cluster_analysis/District/code_0915")
-
+setwd("C:/work/Cluster_Analysis/Karnataka/primary_education_cluster_analysis")
 input_data <- read.csv("District_Data.csv")
 
 ##### Explore Data #####
@@ -114,11 +102,18 @@ input_data_sub <- input_data[, -1]
 ## Scaling the data ##
 input_data_scaled <- scale(input_data_sub)
 
+input_data_scaled <- as.data.frame (scale(input_data[,-1]))
+
+class(input_data_scaled)
+
+row.names(input_data_scaled) <- input_data$District
+
+
 ## adding weights to reading and maths performance colums ##
 
-input_data_scaled[,16] = 3 * input_data_scaled[,16] #3to5_Can_read_1text(%)
+input_data_scaled[,18] = 3 * input_data_scaled[,18] #3to5_Can_read_1text(%)
 
-input_data_scaled[,17] = 3 * input_data_scaled[,17] #3to5_Can_Subtract (%)
+input_data_scaled[,19] = 3 * input_data_scaled[,19] #3to5_Can_Subtract (%)
 
 input_data_scaled[,6] = .2 * input_data_scaled[,6] #3to5_Can_Subtract (%)
 ##
@@ -146,14 +141,16 @@ gap_stat <- clusGap(input_data_scaled, FUN = hcut, K.max = 5, B = 60)
 fviz_gap_stat(gap_stat)
 
 ##Converting distances into a matrix and writing it into a csv file ##
-d <- dist(input_data_scaled, method = "euclidean")
-write.csv(as.matrix(d), "distmatrix.csv", row.names = F)
 
+d <- dist(input_data_scaled, method = "euclidean")
+d <- get_dist(input_data_scaled, method = "euclidean")
+#write.csv(as.matrix(d), "distmatrix.csv", row.names = F)
+## For Visualising the distance matrix
+fviz_dist(d,lab_size = 8)
 
 ## Applying the algorithm ##
 
 clust <-hclust (d, method = "ward.D2")
-clust$labels <- input_data$District
 
 ## Plotting dendogram ##
 
@@ -162,22 +159,38 @@ rect.hclust(clust, 3)
 
 ## Slicing the dendogram to get finite number of clusters ##
 
-k <- cutree(clust, k=3)
-head(k)
+groups <- cutree(clust, k=3)
+values_cat <- c("Tier 2", "Tier 3", "Tier 1")
 
-## attach it to the original dataset and write it to a csv file ##
-input_data$cluster <- k
-write.csv(input_data, "results_clust.csv")
+## attach cluster number and cluster category to the original dataset  ##
+input_data$group <- groups
+
+#index <- c(1, 2, 3)
+input_data$clust_cat <- values_cat[input_data$group]
+
+
+## create a data frame with district, cluster number and cluster category and write it to a csv file.
+perf_data <- data.frame(input_data$District, input_data$group, input_data$clust_cat)
+colnames(perf_data) <-  c("District", "cluster_num", "cluster_cat")
+
+write.csv(perf_data, "clust_info.csv")
+
+## scatter plot to visualise the clusters
+hc.cut <- hcut(input_data_scaled, k=3, hc_method = "ward.D2")
+fviz_dend(hc.cut, show_labels = FALSE, rect = TRUE, show_labels = TRUE)
+
+fviz_cluster(list(data = as.matrix(input_data_scaled), cluster = input_data$clust_cat), 
+             palette = "Set2", ggtheme = theme_minimal(), labelsize = 8)
 
 
 ##### Profiling #####
 
 ## population summaries ##
 options(scripen = 999)
-popMean <- apply(input_data[,2:18],2,mean)
-popSD <- apply(input_data[,2:18],2,sd)
+popMean <- apply(input_data[,2:20],2,mean)
+popSD <- apply(input_data[,2:20],2,sd)
 
-pmeans <- data.frame(matrix(0, ncol = 18, nrow = 1))
+pmeans <- data.frame(matrix(0, ncol = 20, nrow = 1))
 
 names(pmeans) = names(c("Summary",input_data_sub) )
 pmeans <- rbind(pmeans, z= c("Mean", popMean))
@@ -188,7 +201,7 @@ pmeans <- pmeans[-1,]
 write.csv(pmeans,"pmeans.csv",row.names = F)
 
 ## Clusterwise summaries ##
-cmeans <- aggregate(input_data[,2:18], by = list(input_data$cluster), FUN = mean)
+cmeans <- aggregate(input_data[,2:20], by = list(input_data$group), FUN = mean)
 
 clust_col_list <- names(cmeans)
 
@@ -197,15 +210,45 @@ clust_col_list <- names(cmeans)
 
 for(i in 1:length(clust_col_list))
 {
-  y <- (cmeans[,i+1] - apply(input_data[,2:18],2,mean)[i])/ (apply(input_data[,2:18],2,sd)[i])
+  y <- (cmeans[,i+1] - apply(input_data[,2:20],2,mean)[i])/ (apply(input_data[,2:20],2,sd)[i])
   cmeans <- cbind(cmeans,y)
-  names(cmeans)[i+18] <- paste("z", clust_col_list[i+1], sep="_")
+  names(cmeans)[i+20] <- paste("z", clust_col_list[i+1], sep="_")
   print(clust_col_list[i+1])
 }
 
-cmeans <- cmeans[,-(2*18)]
+cmeans <- cmeans[,-(2*20)]
 write.csv(cmeans,"cmeans.csv",row.names = F)
 
 ## Number of records in each cluster ##
-sqldf("select count(*) from input_data group by cluster")
+sqldf("select clust_cat, count(*) as count from input_data group by clust_cat")
+
+
+
+
+#################### Visualization###############################
+
+karnataka.rg <- readOGR(dsn = "shapeFile/district_Output.shp", "district_Output")
+
+DIST <- karnataka.rg$DIST
+
+file_csv<- read.csv("clust_info.csv")
+
+# merge on common variable, here called 'key'
+m <- merge(karnataka.rg, file_csv, by.x='DIST', by.y="District")
+m$DIST = as.factor(m$DIST)
+
+shapefile(m, "shapeFile/merged_district_Output.shp", overwrite = TRUE)
+
+karnataka.rg_m <- readOGR(dsn = "shapeFile/merged_district_Output.shp", "merged_district_Output")
+
+tm_shape(karnataka.rg_m) + tm_polygons("clstr_c", aes.palette = "hclust", palette = "-Blues", contrast = .5, title = "Learning Outcome Clusters", legend.show = TRUE ) + 
+tm_layout(  legend.title.size = 1.5,
+            legend.text.size = 0.5,
+            legend.outside = FALSE,
+            legend.position = c(0,.82),
+            legend.bg.color = "white",
+            legend.bg.alpha = .5
+            )  +
+tm_text("DIST", size = "AREA", scale  = .6)  
+
 
